e15172b6fc794a2600083305b52e0e86d8927869
mathildeolesen <111729553+mathildeolesen@users.noreply.github.com>
2024-02-25 20:41:35 +0100

    Hente og utforming av data fra API (async/await) +  mer Sass
    (Forelesning 13 - 22. februar)
    
    * Hente data fra API (rick and morty API)
    -- async: Skal kjøre asynkront, det vil si ved siden av annen kode. Hvis vi f.eks venter på data som ikke nødvendigvis er tilgjengelig dette .001 sekundet, (data-kall f.eks). Slik kan andre komponenter og kode rendres og kjøre mens vi venter på data.
    -- await: Vent her i koden til ... er fullført. Vi vil f.eks vente på at fetch() er ferdig, slik at vi kan behandle innkommende data ordentlig.
    
    * mer Sass
    -- Nesting og ny partials (_postpage.scss)

--- start/src/App.jsx
+++ start/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 import Home from './components/Home'
@@ -12,18 +12,29 @@ import PostPage from './components/PostPage'
 
 function App() {
 
-  console.log(posts)
+  const [content, setContent] = useState([])
+
+  const getData = async() => {
+    const response = await fetch('https://rickandmortyapi.com/api/character')
+    const data = await response.json()
+    setContent(data.results)
+  } 
+
+  useEffect(()=>{
+    getData()
+  }, [])
+
 
   const [count, setCount] = useState(0)
 
   return (
     <Layout>
       <Routes>
-        <Route index element={<Home/>}/>
+        <Route index element={<Home content={content}/>}/>
         <Route path="categories/*" element={<Categories/>}>
           <Route index element={<CategoriesIndex/>}/>
-          <Route path=":slug" element={<Category/>}/>
-          <Route path=":slug/:postid" element={<PostPage/>}/>
+          <Route path=":slug" element={<Category posts={posts} />}/>
+          <Route path=":slug/:postid" element={<PostPage posts={posts} />}/>
         </Route>
       </Routes>
     </Layout>
